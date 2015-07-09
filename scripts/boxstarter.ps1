f7d55ba1b9a9b6b1f078dd8adf1714d876e1c6a3
Matt Wrock <matt@mattwrock.com>
2015-07-08 23:48:19 -0700

    import boxstarter before call

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -4,4 +4,5 @@ Get-Boxstarter -Force
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 
+Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 Install-BoxstarterPackage -PackageName a:\package.ps1 -Credential $cred
