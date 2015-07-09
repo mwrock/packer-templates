2405cefaf776cc8bb7a5a86278edcac935d3a000
Matt Wrock <matt@mattwrock.com>
2015-07-09 07:05:01 -0700

    adding messaging, removing features and winupdates

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -1,6 +1,7 @@
 Enable-RemoteDesktop
 Set-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP -Enabled True
 
+Write-BoxstarterMessage "Shrinking page file"
 $System = GWMI Win32_ComputerSystem -EnableAllPrivileges
 $System.AutomaticManagedPagefile = $False
 $System.Put()
@@ -12,17 +13,27 @@ $CurrentPageFile.Put()
 
 Update-ExecutionPolicy -Policy Unrestricted
 
-# Install-WindowsUpdate -AcceptEula
+Write-BoxstarterMessage "Removing unused features..."
+Remove-WindowsFeature -Name 'Powershell-ISE'
+Get-WindowsFeature | 
+? { $_.InstallState -eq 'Available' } | 
+Uninstall-WindowsFeature -Remove
 
+Install-WindowsUpdate -AcceptEula
+
+Write-BoxstarterMessage "Cleaning SxS..."
 Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase
 
+Write-BoxstarterMessage "0ing out empty space..."
 wget http://download.sysinternals.com/files/SDelete.zip -OutFile sdelete.zip
 [System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem")
 [System.IO.Compression.ZipFile]::ExtractToDirectory("sdelete.zip", ".") 
 ./sdelete.exe /accepteula -z c: 
 
+Write-BoxstarterMessage "defragging..."
 Optimize-Volume -DriveLetter C
 
+Write-BoxstarterMessage "Setting up winrm"
 Set-NetFirewallRule -Name WINRM-HTTP-In-TCP-PUBLIC -RemoteAddress Any
 Enable-WSManCredSSP -Force -Role Server
 
