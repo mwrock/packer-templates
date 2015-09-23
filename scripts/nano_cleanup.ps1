3aef95aa5da42ffeec526a5a9f19e11ae9409e1d
Matt Wrock <matt@mattwrock.com>
2015-09-23 09:05:47 -0700

    powershell implementation of sdelete

--- scripts/nano_cleanup.ps1
+++ scripts/nano_cleanup.ps1
@@ -12,6 +12,26 @@ Resize-Partition -DriveLetter C -Size ($c_size + $d_size)
 
 Optimize-Volume -DriveLetter C
 
-# C:\Sdelete\sdelete.exe /accepteula -z c:
+$FilePath="c:\zero.tmp"
+$Volume= Get-Volume -DriveLetter C
+$ArraySize= 64kb
+$SpaceToLeave= $Volume.Size * 0.05
+$FileSize= $Volume.SizeRemaining - $SpacetoLeave
+$ZeroArray= new-object byte[]($ArraySize)
+ 
+$Stream= [io.File]::OpenWrite($FilePath)
+try {
+   $CurFileSize = 0
+    while($CurFileSize -lt $FileSize) {
+        $Stream.Write($ZeroArray,0, $ZeroArray.Length)
+        $CurFileSize +=$ZeroArray.Length
+    }
+} finally {
+    if($Stream) {
+        $Stream.Close()
+    }
+}
+ 
+Del $FilePath
 
-# shutdown /s /t 0
+shutdown /s /t 0
