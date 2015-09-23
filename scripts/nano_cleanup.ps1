0d28d92d699aac9835b983e18663abdad549044a
Matt Wrock <matt@mattwrock.com>
2015-09-23 08:21:44 -0700

    no autoloading in setup complete so load everything and disable sdelete and shutdown while we troubleshoot

--- scripts/nano_cleanup.ps1
+++ scripts/nano_cleanup.ps1
@@ -1,3 +1,7 @@
+Import-Module C:\windows\system32\windowspowershell\v1.0\Modules\Microsoft.PowerShell.Utility\Microsoft.PowerShell.Utility.psd1
+Import-Module C:\windows\system32\windowspowershell\v1.0\Modules\Microsoft.PowerShell.Management\Microsoft.PowerShell.Management.psd1
+Import-Module C:\windows\system32\windowspowershell\v1.0\Modules\Storage\Storage.psd1
+
 $partition = Get-Partition -DriveLetter C
 $c_size = $partition.size
 $partition = Get-Partition -DriveLetter D
@@ -8,4 +12,6 @@ Resize-Partition -DriveLetter C -Size ($c_size + $d_size)
 
 Optimize-Volume -DriveLetter C
 
-C:\Sdelete\sdelete.exe /accepteula -z c:
+# C:\Sdelete\sdelete.exe /accepteula -z c:
+
+# shutdown /s /t 0
