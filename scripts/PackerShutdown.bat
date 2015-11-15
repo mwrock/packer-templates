e14ea47103e8b01c5c18e9d2d868a6fc55a09df4
Matt Wrock <matt@mattwrock.com>
2015-11-14 17:43:08 -0800

    fix firewall rule set

--- scripts/PackerShutdown.bat
+++ scripts/PackerShutdown.bat
@@ -1,2 +1,2 @@
-netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=block
+netsh advfirewall firewall set rule name="WinRM-HTTP" new action=block
 C:/windows/system32/sysprep/sysprep.exe /generalize /oobe /unattend:C:/Windows/Panther/Unattend/unattend.xml /quiet /shutdown
