1d566cfd047072a1e8f69aa51ab4e3fc4f92aa78
Matt Wrock <matt@mattwrock.com>
2016-07-24 09:05:21 -0700

    initial chef implementation

---
+++ cookbooks/2012r2/metadata.rb
@@ -0,0 +1,7 @@
+name '2012r2'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures 2012r2'
+long_description 'Installs/Configures 2012r2'
+version '0.1.0'
---
+++ scripts/winrm.ps1
@@ -0,0 +1,3 @@
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
