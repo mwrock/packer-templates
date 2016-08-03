d979318aa84ce6928699761b7ec97a21771ce644
Matt Wrock <matt@mattwrock.com>
2016-08-03 04:26:29 -0700

    fix vbox guest additions

--- cookbooks/2012r2/recipes/install_ps_modules.rb
+++ cookbooks/2012r2/recipes/install_ps_modules.rb
@@ -3,7 +3,7 @@ powershell_script 'install Nuget package provider' do
   not_if '(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue) -ne $null'
 end
 
-%w{PSWindowsUpdate xNetworking xRemoteDesktopAdmin}.each do |ps_module|
+%w{PSWindowsUpdate xNetworking xRemoteDesktopAdmin xCertificate}.each do |ps_module|
   powershell_script "install #{ps_module} module" do
     code "Install-Module #{ps_module} -Force"
     not_if "(Get-Module #{ps_module} -list) -ne $null"
--- cookbooks/2012r2/recipes/vbox_guest_additions.rb
+++ cookbooks/2012r2/recipes/vbox_guest_additions.rb
@@ -2,10 +2,16 @@ cookbook_file "#{ENV['temp']}/oracle.cer" do
   source "oracle.cer"
 end
 
-windows_certificate "#{ENV['temp']}/oracle.cer" do
-    store_name  "MY"
+dsc_resource "Install Oracle Cert" do
+  resource :xCertificateImport
+  property :Thumbprint, "7e92b66be51b79d8ce3ff25c15c2df6ab8c7f2f2"
+  property :Store, "TrustedPublisher"
+  property :Location, "LocalMachine"
+  property :Path, "#{ENV['temp']}/oracle.cer"
 end
 
 package "virtual box guest additions" do
   source "e:/VBoxWindowsAdditions.exe"
+  installer_type :custom
+  options "/S"
 end
