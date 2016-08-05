82900e21162d3c52e98ebfc5262f7a183263017a
Matt Wrock <matt@mattwrock.com>
2016-08-04 22:51:44 -0700

    fix firewall change

--- cookbooks/2012r2/recipes/add_setup_complete.rb
+++ cookbooks/2012r2/recipes/add_setup_complete.rb
@@ -3,6 +3,6 @@ directory "C:/Windows/setup/scripts" do
 end
 
 file "restart winmgmt on first boot" do
-  content "netsh advfirewall firewall set rule name='WinRM-HTTP' new action=allow"
+  content "netsh advfirewall firewall set rule name=\"WinRM-HTTP\" new action=allow"
   path "C:/Windows/setup/scripts/SetupComplete.cmd"
 end
