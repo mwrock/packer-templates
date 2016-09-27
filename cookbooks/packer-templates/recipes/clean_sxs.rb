a1c04b565cb603db3a0bcd83a9a96f855381ac82
Matt Wrock <matt@mattwrock.com>
2016-09-26 23:08:51 -0700

    fixing 2016

--- cookbooks/packer-templates/recipes/clean_sxs.rb
+++ cookbooks/packer-templates/recipes/clean_sxs.rb
@@ -1,3 +1,7 @@
+batch 'restore sxs health' do
+  code 'Dism.exe /online /Cleanup-Image /RestoreHealth'
+end
+
 batch 'clean SxS' do
   code 'Dism.exe /online /Cleanup-Image /StartComponentCleanup /ResetBase'
 end
