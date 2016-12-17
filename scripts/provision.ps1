cf943dcc2b463ad3d98f3d51a9d32b1b53f622a8
Matt Wrock <matt@mattwrock.com>
2016-12-17 11:39:24 -0800

    use attach instead of upload for vbox guest additions on 2012r2

--- scripts/provision.ps1
+++ scripts/provision.ps1
@@ -7,14 +7,10 @@ netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=
 if(Test-Path "C:\Users\vagrant\VBoxGuestAdditions.iso") {
     Write-Host "Installing Guest Additions"
     certutil -addstore -f "TrustedPublisher" A:\oracle.cer
-    cinst 7zip.commandline -y
-    Move-Item C:\Users\vagrant\VBoxGuestAdditions.iso C:\Windows\Temp
-    ."C:\ProgramData\chocolatey\lib\7zip.commandline\tools\7z.exe" x C:\Windows\Temp\VBoxGuestAdditions.iso -oC:\Windows\Temp\virtualbox
 
-    Start-Process -FilePath "C:\Windows\Temp\virtualbox\VBoxWindowsAdditions.exe" -ArgumentList "/S" -WorkingDirectory "C:\Windows\Temp\virtualbox" -Wait
+    Start-Process -FilePath "e:/VBoxWindowsAdditions.exe" -ArgumentList "/S" -WorkingDirectory "C:\Windows\Temp\virtualbox" -Wait
 
     Remove-Item C:\Windows\Temp\virtualbox -Recurse -Force
-    Remove-Item C:\Windows\Temp\VBoxGuestAdditions.iso -Force
 }
 
 Write-Host "Cleaning SxS..."
