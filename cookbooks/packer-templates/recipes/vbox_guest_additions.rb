334f248371a8191f97d640066ec5c5aad2a740f2
Matt Wrock <matt@mattwrock.com>
2017-01-03 01:51:50 -0800

    use cert in vbox additions iso

--- cookbooks/packer-templates/recipes/vbox_guest_additions.rb
+++ cookbooks/packer-templates/recipes/vbox_guest_additions.rb
@@ -1,13 +1,9 @@
-cookbook_file "#{ENV['temp']}/oracle.cer" do
-  source "oracle.cer"
-end
-
 dsc_resource "Install Oracle Cert" do
   resource :xCertificateImport
   property :Thumbprint, "7e92b66be51b79d8ce3ff25c15c2df6ab8c7f2f2"
   property :Store, "TrustedPublisher"
   property :Location, "LocalMachine"
-  property :Path, "#{ENV['temp']}/oracle.cer"
+  property :Path, "e:/cert/vbox-sha1.cer"
 end
 
 package "virtual box guest additions" do
