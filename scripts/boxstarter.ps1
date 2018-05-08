04a5befd8937538c233e11ff6c412a19ca5396e3
AJ Suchocki <asuchocki@AJs-MacBook-Pro.local>
2018-05-08 16:22:50 -0700

    Added 2012 (non R2) support

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -8,5 +8,5 @@ Get-Boxstarter -Force
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 
-Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
+Import-Module $env:programdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 Install-BoxstarterPackage -PackageName a:\package.ps1 -Credential $cred
