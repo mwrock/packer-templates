8fa651dac8a56d8f33df045bb35e0517eee28a35
Uri Simchoni <urisimchoni@gmail.com>
2020-04-17 08:04:09 +0300

    Win2016 images - fix TLS protocol issue to allow installation of NuGet
    https://rnelson0.com/2018/05/17/powershell-in-a-post-tls1-1-world/

--- cookbooks/packer-templates/recipes/install_ps_modules.rb
+++ cookbooks/packer-templates/recipes/install_ps_modules.rb
@@ -1,11 +1,17 @@
 powershell_script 'install Nuget package provider' do
-  code 'Install-PackageProvider -Name NuGet -Force'
+  code <<-EOH
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Install-PackageProvider -Name NuGet -Force
+  EOH
   not_if '(Get-PackageProvider -Name Nuget -ListAvailable -ErrorAction SilentlyContinue) -ne $null'
 end
 
 %w{PSWindowsUpdate xNetworking xRemoteDesktopAdmin xCertificate}.each do |ps_module|
   powershell_script "install #{ps_module} module" do
-    code "Install-Module #{ps_module} -Force"
+    code <<-EOH
+      [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+      Install-Module #{ps_module} -Force
+    EOH
     not_if "(Get-Module #{ps_module} -list) -ne $null"
   end
 end
